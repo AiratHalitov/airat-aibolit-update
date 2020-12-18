e870c79fb91a4ba8b6833633e2d292cd88e24a0a
Airat Halitov <airat.halitov@gmail.com>
2020-12-18 11:59:06 +0500

    rm -rf

--- airat-aibolit-update.sh
+++ airat-aibolit-update.sh
@@ -5,7 +5,7 @@ rm -rf ai-bolit-hoster.zip ai-bolit tools changelog.txt readme.txt
 wget https://download.cloudscan.tech:28080/partners/ai-bolit-hoster.zip > /dev/null 2>&1
 
 if [ -f ai-bolit-hoster.zip ]; then
-    unzip ai-bolit-hoster.zip > /dev/null 2>&1 && rm ai-bolit-hoster.zip
+    unzip ai-bolit-hoster.zip > /dev/null 2>&1 && rm -rf ai-bolit-hoster.zip
     # rm -rf changelog.txt readme.txt
     cp run-aibolit.sh ai-bolit/run-aibolit.sh && chmod +x ai-bolit/run-aibolit.sh
     echo "Done!"
