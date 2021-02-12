7de2c5cfaa301d3041933c0020678184c85689a9
AiratHalitov <airat.halitov@gmail.com>
2021-02-12 09:15:51 +0500

    Keep zip-archive

--- airat-aibolit-update.sh
+++ airat-aibolit-update.sh
@@ -10,8 +10,8 @@ wget -q https://download.cloudscan.tech:28080/partners/ai-bolit-hoster.zip
 
 if [ -f ai-bolit-hoster.zip ]; then
     rm -rf ai-bolit tools changelog.txt readme.txt
-    unzip -q ai-bolit-hoster.zip && rm -rf ai-bolit-hoster.zip
-    # rm -rf changelog.txt readme.txt
+    unzip -q ai-bolit-hoster.zip
+    # rm -rf changelog.txt readme.txt ai-bolit-hoster.zip
     cp run-aibolit.sh ai-bolit/run-aibolit.sh && chmod +x ai-bolit/run-aibolit.sh
     echo "Done!"
 fi
