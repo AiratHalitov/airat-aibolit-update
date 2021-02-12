85864ea03ef023f00285a6a5c0ed113439311d6a
AiratHalitov <airat.halitov@gmail.com>
2021-02-12 09:05:16 +0500

    Protect from deleting

--- airat-aibolit-update.sh
+++ airat-aibolit-update.sh
@@ -4,11 +4,12 @@
 # GitHub: https://github.com/AiratHalitov/airat-aibolit-update
 # License: GPLv3
 
-rm -rf ai-bolit-hoster.zip ai-bolit tools changelog.txt readme.txt
+rm -rf ai-bolit-hoster.zip
 
 wget -q https://download.cloudscan.tech:28080/partners/ai-bolit-hoster.zip
 
 if [ -f ai-bolit-hoster.zip ]; then
+    rm -rf ai-bolit tools changelog.txt readme.txt
     unzip -q ai-bolit-hoster.zip && rm -rf ai-bolit-hoster.zip
     # rm -rf changelog.txt readme.txt
     cp run-aibolit.sh ai-bolit/run-aibolit.sh && chmod +x ai-bolit/run-aibolit.sh
